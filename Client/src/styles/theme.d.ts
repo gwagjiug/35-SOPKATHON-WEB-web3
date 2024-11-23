fa12cd1365877e20dcb0c931a64f407850fbb738
daahyunk <ekgusdl0408@naver.com>
2024-11-24 01:25:53 +0900

    init: theme 폰트 설정

---
+++ Client/src/styles/theme.d.ts
@@ -0,0 +1,33 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    color: {
+      gray01: string;
+      gray02: string;
+      gray03: string;
+      gray04: string;
+      gray05: string;
+      gray06: string;
+      gray07: string;
+      gray08: string;
+      gray09: string;
+      gray10: string;
+      white: string;
+      black: string;
+    };
+    main: {
+      main01: string;
+      main02: string;
+      main03: string;
+      main04: string;
+    };
+    font: {
+      title_sb_24: string;
+      title_sb_16: string;
+      title_sb_20: string;
+      title_sb_32: string;
+      title_sb_14: string;
+    };
+  }
+}
