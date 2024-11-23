1603a10a03a1c39712caddffa659183ff599445c
goeun kim <rlarhdms6828@naver.com>
2024-11-24 04:51:36 +0900

    Design: list 컴포넌트 생성

--- Client/src/pages/List.jsx
+++ Client/src/pages/List.jsx
@@ -1,11 +1,9 @@
 import MobileLayout from '../components/common/MobileLayout';
 
 const List = () => {
-  return (
-    <MobileLayout>
-      <main>이전의 루틴 기록을 볼 수 있는 리스트 페이지입니다.</main>
-    </MobileLayout>
-  );
+  return <MobileLayout></MobileLayout>;
 };
 
 export default List;
+
+c;
