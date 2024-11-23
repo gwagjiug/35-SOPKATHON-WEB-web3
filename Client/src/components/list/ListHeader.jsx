ff49c55c33edf59fa42641e12aacf0e8bd612447
daahyunk <ekgusdl0408@naver.com>
2024-11-24 03:06:38 +0900

    feat: 리스트 헤더 구현

--- Client/src/components/list/ListHeader.jsx
+++ Client/src/components/list/ListHeader.jsx
@@ -1,59 +1,33 @@
 import styled from 'styled-components';
-import PropTypes from 'prop-types';
+import { IcnRoutineBack } from '../../assets/svg';
 
-const HeaderContainer = styled.header`
-  display: flex;
-  align-items: center;
-  padding: 16px;
-  background-color: #fff;
-  border: 1px solid red;
-  width: 100%;
-`;
-
-const BackButton = styled.button`
-  background: none;
-  border: none;
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-  margin-right: 8px;
-`;
-
-const BackIcon = styled.svg`
-  width: 24px;
-  height: 24px;
-`;
-
-const Title = styled.h1`
-  font-size: 16px;
-  font-weight: bold;
-  color: #333;
-`;
-
+/* eslint-disable react/prop-types */
 const ListHeader = ({ title, onBack }) => {
   return (
     <HeaderContainer>
-      <BackButton onClick={onBack}>
-        <BackIcon viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          {/* Replace this with your actual SVG */}
-          <path
-            d="M15 18l-6-6 6-6"
-            stroke="currentColor"
-            strokeWidth="2"
-            fill="none"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </BackIcon>
-      </BackButton>
+      <StyledIcnRoutineBack onClick={onBack}/>
       <Title>{title}</Title>
     </HeaderContainer>
   );
 };
 
-ListHeader.propTypes = {
-  title: PropTypes.string.isRequired,
-  onBack: PropTypes.func.isRequired,
-};
+const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  padding: 2rem;
+  background-color: ${({ theme }) => theme.color.white};
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  ${({ theme }) => theme.font.title_sb_14}
+  color: ${({ theme }) => theme.color.gray08};
+  margin-left: 0.4rem;
+`;
+
+const StyledIcnRoutineBack = styled(IcnRoutineBack)`
+  width: 2rem;
+  height: 2rem;
+`;
 
 export default ListHeader;
