fdf127cb568e2b9b3fdb0b93a1cd0154cd3639e6
goeun kim <rlarhdms6828@naver.com>
2024-11-23 23:33:42 +0900

    Design: 메인 페이지 틀

--- Client/src/pages/Main.jsx
+++ Client/src/pages/Main.jsx
@@ -3,7 +3,16 @@ import MobileLayout from '../components/common/MobileLayout';
 const Main = () => {
   return (
     <MobileLayout>
-      <main>제일 처음 보여지는 메인페이지입니다</main>
+      <main>
+        <div>
+          <div>
+            <h1>나는야 빡빡이</h1>
+            <h2>볼디의 머리를 채워주세요!</h2>
+          </div>
+          <img></img>
+        </div>
+        <button></button>
+      </main>
     </MobileLayout>
   );
 };
