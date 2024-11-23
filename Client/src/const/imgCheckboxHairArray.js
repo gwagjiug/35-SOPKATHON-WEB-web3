f8bb1484204f24ca55685742d31e5c78c1891301
daahyunk <ekgusdl0408@naver.com>
2024-11-24 06:46:31 +0900

    feat: 빡빡이 배열 생성

---
+++ Client/src/const/imgCheckboxHairArray.js
@@ -0,0 +1,20 @@
+import { 
+    ImgCheckboxHair01,
+    ImgCheckboxHair02,
+    ImgCheckboxHair04,
+    ImgCheckboxHair05,
+    ImgCheckboxHair06,
+    ImgCheckboxHair07,
+    ImgCheckboxHair08,
+  } from '../assets/svg/index'; 
+  
+export const ImgCheckboxHairArray = [
+    ImgCheckboxHair01,
+    ImgCheckboxHair02,
+    ImgCheckboxHair04,
+    ImgCheckboxHair05,
+    ImgCheckboxHair06,
+    ImgCheckboxHair07,
+    ImgCheckboxHair08,
+  ];
+  
